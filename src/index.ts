747e4c8c072215d11d3ae62b225de33b5a2cc59e
dinesh1042 <dineshram1042002@gmail.com>
2023-01-26 22:49:21 +0530

    Initialize express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Insta")
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3007;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
